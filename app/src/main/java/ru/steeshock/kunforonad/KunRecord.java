62448dcaa67bbbda967af8641d21bc28864495df
Eugeny <steeshock@gmail.com>
2018-03-21 21:47:13 +0300

    Adding Clear fields feature.

---
+++ app/src/main/java/ru/steeshock/kunforonad/KunRecord.java
@@ -0,0 +1,16 @@
+package ru.steeshock.kunforonad;
+
+import java.io.Serializable;
+
+/**
+ * Created by steeshock on 18.03.2018.
+ */
+
+public class KunRecord  implements Serializable{
+
+     String date;
+     String stage;
+     String state;
+     String series;
+
+}
