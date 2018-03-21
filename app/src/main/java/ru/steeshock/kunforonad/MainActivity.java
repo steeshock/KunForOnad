6f67af8ad9edf11df850c535f391ae1e5b37c455
Eugeny <steeshock@gmail.com>
2018-03-21 21:45:07 +0300

    Adding Clear fields feature.

--- app/src/main/java/ru/steeshock/kunforonad/MainActivity.java
+++ app/src/main/java/ru/steeshock/kunforonad/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.steeshock.kunforonad;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
@@ -8,17 +10,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity{
 
-    Calendar dateAndTime = Calendar.getInstance();
-    EditText etDate, et_State, et_Series, et_pName, et_pOboz, et_pNumber, et_pDesc,
+    private Calendar dateAndTime = Calendar.getInstance();
+
+    private Spinner sp_stage;
+    private EditText et_Date, et_State, et_Series, et_pName, et_pOboz, et_pNumber, et_pDesc,
             et_bName, et_bOboz, et_bNumber, et_bDesc, et_Position, et_positionDesc,
             et_el1_1, et_el2_1, et_el3_1, et_el4_1, et_el1_2, et_el2_2, et_el3_2, et_el4_2,
             et_analysisResult, et_Reason, et_Fault, et_Ai, et_Protocol;
-    Button btnClear, btnSave;
+
+    private KunRecord mKun = new KunRecord();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity{
 
              //INITIALIZING COMPONENTS//
 
-        etDate = findViewById(R.id.et_date);
+        et_Date = findViewById(R.id.et_date);
         et_State = findViewById(R.id.et_state);
         et_Series = findViewById(R.id.et_series);
         et_pName = findViewById(R.id.et_pName);
@@ -54,15 +60,17 @@ public class MainActivity extends AppCompatActivity{
         et_Ai = findViewById(R.id.et_Ai);
         et_Protocol = findViewById(R.id.et_Protocol);
 
-        btnClear = findViewById(R.id.clear);
-        btnSave = findViewById(R.id.save);
+        sp_stage = findViewById(R.id.sp_stage);
+
+        Button btnClear = findViewById(R.id.clear);
+        Button btnSave = findViewById(R.id.save);
 
         btnClear.setOnClickListener(mOnClearClickListener);
+        btnSave.setOnClickListener(mOnSaveClickListener);
 
         setInitialDateTime();
 
     }
-
 
     // отображаем диалоговое окно для выбора даты
     public void setDate(View v) {
@@ -77,7 +85,7 @@ public class MainActivity extends AppCompatActivity{
     private void setInitialDateTime() {
         //String editTextDateParam = dateAndTime.get(Calendar.DAY_OF_MONTH) + "." + (dateAndTime.get(Calendar.MONTH) + 1) + "." + dateAndTime.get(Calendar.YEAR);
         //etDate.setText(editTextDateParam);
-        etDate.setText(DateUtils.formatDateTime(this,
+        et_Date.setText(DateUtils.formatDateTime(this,
                 dateAndTime.getTimeInMillis(),
                 DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
                         | DateUtils.FORMAT_SHOW_TIME));
@@ -91,39 +99,81 @@ public class MainActivity extends AppCompatActivity{
             setInitialDateTime();
         }
     };
+
     // очистка всех полей ввода
     View.OnClickListener mOnClearClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            et_State.getText().clear();
-            et_Series.getText().clear();
-            et_pName.getText().clear();
-            et_pOboz.getText().clear();
-            et_pNumber.getText().clear();
-            et_pDesc.getText().clear();
-            et_bName.getText().clear();
-            et_bOboz.getText().clear();
-            et_bNumber.getText().clear();
-            et_bNumber.getText().clear();
-            et_bDesc.getText().clear();
-            et_Position.getText().clear();
-            et_positionDesc.getText().clear();
-            et_el1_1.getText().clear();
-            et_el2_1.getText().clear();
-            et_el3_1.getText().clear();
-            et_el4_1.getText().clear();
-            et_el1_2.getText().clear();
-            et_el2_2.getText().clear();
-            et_el3_2.getText().clear();
-            et_el4_2.getText().clear();
-            et_analysisResult.getText().clear();
-            et_Reason.getText().clear();
-            et_Fault.getText().clear();
-            et_Ai.getText().clear();
-            et_Protocol.getText().clear();
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Внимание!")
+                    .setMessage("Вся введенная информация будет удалена. Вы уверены?")
+                    .setCancelable(false)
+                    .setIcon(R.drawable.cancel)
+                    .setNegativeButton("ДА",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    clearRecord();
+                                }
+                            })
+                    .setPositiveButton("НЕТ",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
         }
     };
 
+    // сохранения КУНа в БД
+    View.OnClickListener mOnSaveClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
+            saveRecord();
+        }
+    };
+
+    public void saveRecord () {
+
+        mKun.date = et_Date.getText().toString();
+        mKun.stage = sp_stage.getSelectedItem().toString();
+        mKun.state = et_State.getText().toString();
+        mKun.series = et_Series.getText().toString();
+
+        et_pDesc.setText(mKun.toString());
+
+    }
+
+    public void clearRecord () {
+        et_State.getText().clear();
+        et_Series.getText().clear();
+        et_pName.getText().clear();
+        et_pOboz.getText().clear();
+        et_pNumber.getText().clear();
+        et_pDesc.getText().clear();
+        et_bName.getText().clear();
+        et_bOboz.getText().clear();
+        et_bNumber.getText().clear();
+        et_bNumber.getText().clear();
+        et_bDesc.getText().clear();
+        et_Position.getText().clear();
+        et_positionDesc.getText().clear();
+        et_el1_1.getText().clear();
+        et_el2_1.getText().clear();
+        et_el3_1.getText().clear();
+        et_el4_1.getText().clear();
+        et_el1_2.getText().clear();
+        et_el2_2.getText().clear();
+        et_el3_2.getText().clear();
+        et_el4_2.getText().clear();
+        et_analysisResult.getText().clear();
+        et_Reason.getText().clear();
+        et_Fault.getText().clear();
+        et_Ai.getText().clear();
+        et_Protocol.getText().clear();
+    }
 
 }
